5762bb00de65a7cec456018ff00d49689857ae2e
laixiansong <laixiansong@2218474399.com>
2021-12-01 14:29:18 +0800

    test

--- source_code/draft.py
+++ source_code/draft.py
@@ -2,8 +2,14 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture("../resources/eye/eye_closed_01.avi")
-while True:
-    ret, image = cap.read()
-    cv2.imshow("windows", image)
+while(True):
+    ret, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("windows", gray)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
+cap.release()
+cv2.destropAllWindows()
