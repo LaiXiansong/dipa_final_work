f7829ef48b10bd68b4e78e3da85369ba8a679d66
LaiXiansong <2218474399@qq.com>
2021-12-03 19:08:48 +0800

    Merge branch 'main' of github.com:LaiXiansong/dipa_final_work

--- source_code/draft.py
+++ source_code/draft.py
@@ -47,8 +47,30 @@ def motionDiff(current_frame, diff):
     out = abs(np.sign(current_frame * diff))*255
     return out
 
+##马赛克
+def do_mosaic(frame, x, y, w, h, neighbor=9):
+    """
+    马赛克的实现原理是把图像上某个像素点一定范围邻域内的所有点用邻域内左上像素点的颜色代替，这样可以模糊细节，但是可以保留大体的轮廓。
+    :param frame: opencv frame
+    :param int x :  马赛克左顶点
+    :param int y:  马赛克右顶点
+    :param int w:  马赛克宽
+    :param int h:  马赛克高
+    :param int neighbor:  马赛克每一块的宽
+    """
+    fh, fw = frame.shape[0], frame.shape[1]
+    if (y + h > fh) or (x + w > fw):
+        return
+    for i in range(0, h - neighbor, neighbor):  # 关键点0 减去neightbour 防止溢出
+        for j in range(0, w - neighbor, neighbor):
+            rect = [j + x, i + y, neighbor, neighbor]
+            color = frame[i + y][j + x].tolist()  # 关键点1 tolist
+            left_up = (rect[0], rect[1])
+            right_down = (rect[0] + neighbor - 1, rect[1] + neighbor - 1)  # 关键点2 减去一个像素
+            cv2.rectangle(frame, left_up, right_down, color, -1)
 
-cap = cv2.VideoCapture("../resources/eye/eye_closed_02.avi")
+
+cap = cv2.VideoCapture("../resources/eye/eye_closed_01.avi")
 start_time = time.time()
 counter = 0 
 fps = cap.get(cv2.CAP_PROP_FPS) #视频平均帧率
@@ -106,8 +128,11 @@ while cap.isOpened():
         
         # frame = cv2.blur(frame, (1, 1))
 
-        frame = cv2.medianBlur(frame, 3)
-        out_image = cv2.Canny(frame,30, 100)
+        # frame = cv2.medianBlur(frame, 3)
+        # out_image = cv2.Canny(frame,30, 100)
+
+        out_image = frame.copy()
+        do_mosaic(out_image, 0, 0, 900, 700, 16)
 
         # ------------------------------------------------------
         cv2.imshow('frame', out_image)
